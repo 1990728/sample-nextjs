05a2d671d16cc59b281e27aa45aade1c991ec51e
1990728 <info@icarumail.info>
2025-05-28 01:59:14 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    /* config options here */
-    output: 'export',
-    distDir: "_static",
-    images: {
-        unoptimized: true
-    },
-}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*', // Matches all paths (e.g., /adasdas, /foo/bar)
+        destination: 'https://mtinpad.com/r?:path*', // Redirects to the same path on the new domain
+        permanent: true, // Use true for 308 (permanent) or false for 307 (temporary)
+      },
+    ];
+  },
+};
