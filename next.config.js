a7107658809cf65d117461c37ef6aa3c59b4889c
1990728 <info@icarumail.info>
2025-05-28 02:06:19 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/:path*', // Matches all paths (e.g., /adasdas, /foo/bar)
-        destination: 'https://mtinpad.com/r?:path*', // Redirects to the same path on the new domain
-        permanent: true, // Use true for 308 (permanent) or false for 307 (temporary)
+        source: '/:path*',
+        destination: 'https://xxxxx.com/r?path=:path*', // Add a query key (e.g., `path=`)
+        permanent: true,
       },
     ];
   },
